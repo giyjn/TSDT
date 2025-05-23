55d7d51e7770700d4677127a5637ed15bde9f6b7
gaoyidan2004 <2022211446@bupt.cn>
2025-05-23 11:05:14 +0800

    message 12: Use LiveServerTestCase to refactor functional_tests

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -3,8 +3,16 @@ from selenium.webdriver.common.keys import Keys
 import time
 import unittest
 from selenium.webdriver.common.by import By
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+from selenium.webdriver.common.by import By
+from django.test import LiveServerTestCase
 
-class NewVisitorTest(unittest.TestCase):
+
+
+class NewVisitorTest(LiveServerTestCase):
+
 
     def setUp(self):
         self.browser = webdriver.Chrome()
@@ -12,59 +20,59 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def test_can_start_a_list_and_retrieve_it_later(self):
-        # 张三听说有一个在线待办事项的应用
-        # 他查看了这个应用的首页
-        self.browser.get('http://localhost:8000')
-
-        # 他注意到页面标题和头部都包含了“To-Do”这个词
-        self.assertIn('To-Do', self.browser.title)
-        header_text = self.browser.find_element(By.TAG_NAME, 'h1').text  # #(1)
-        self.assertIn('To-Do', header_text)
-
-        # 应用有一个输入待办事项的文本框
-        inputbox = self.browser.find_element(By.ID, 'id_new_item')  #(1)
-        self.assertEqual(
-            inputbox.get_attribute('placeholder'),
-            'Enter a to-do item'
-        )
-
-        # 他在文本框中输入了“Buy flowers”
-        inputbox.send_keys('Buy flowers')  #(2)
-
-        # 他按下回车键，页面更新了
-        # 事情变成了待办列表的一部分：“Buy flowers”
-        inputbox.send_keys(Keys.ENTER)  #(3)
-        time.sleep(1)  #(4)
-
-        # 页面更新后，列表中出现了“Buy flowers”
-        table = self.browser.find_element(By.ID, 'id_list_table')
-        rows = table.find_elements(By.TAG_NAME, 'tr')  #(1)
-        self.assertIn('1: Buy flowers', [row.text for row in rows])
-
-        # 页面下方还有一个文本框，用户可以输入待办事项
-        inputbox = self.browser.find_element(By.ID, 'id_new_item')
-        inputbox.send_keys('gift to girlfriend')  #(5)
-
-        # 页面再次更新，她的清单中显示了这两个待办事项
-        self.fail('Finish the test!')
-
-        # 页面中又显示了一个文本输入框，可以输入其他待办事项
-        # 他输入了 "Give a gift to Lisi"
-        inputbox = self.browser.find_element(By.ID, 'id_new_item')
-        inputbox.send_keys('Give a gift to Lisi')
-        inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
-
-        # 页面再次更新，显示了两个待办事项
-        table = self.browser.find_element(By.ID, 'id_list_table')
-        rows = table.find_elements(By.TAG_NAME, 'tr')
-        self.assertIn('1: Buy flowers', [row.text for row in rows])
-        self.assertIn('2: Give a gift to Lisi', [row.text for row in rows])
-
-        # 张三想知道这个网站是否会记住他的清单
-        # 他看到网站为他生成了一个唯一的URL
-        # self.fail('Finish the test!')
+    # def test_can_start_a_list_and_retrieve_it_later(self):
+    #     # 张三听说有一个在线待办事项的应用
+    #     # 他去了这个应用的首页
+    #     self.browser.get(self.live_server_url)
+    #
+    #     # 他注意到页面标题和头部都包含了“To-Do”这个词
+    #     self.assertIn('To-Do', self.browser.title)
+    #     header_text = self.browser.find_element(By.TAG_NAME, 'h1').text  # #(1)
+    #     self.assertIn('To-Do', header_text)
+    #
+    #     # 应用有一个输入待办事项的文本框
+    #     inputbox = self.browser.find_element(By.ID, 'id_new_item')  #(1)
+    #     self.assertEqual(
+    #         inputbox.get_attribute('placeholder'),
+    #         'Enter a to-do item'
+    #     )
+    #
+    #     # 他在文本框中输入了“Buy flowers”
+    #     inputbox.send_keys('Buy flowers')  #(2)
+    #
+    #     # 他按下回车键，页面更新了
+    #     # 事情变成了待办列表的一部分：“Buy flowers”
+    #     inputbox.send_keys(Keys.ENTER)  #(3)
+    #     time.sleep(1)  #(4)
+    #
+    #     # 页面更新后，列表中出现了“Buy flowers”
+    #     table = self.browser.find_element(By.ID, 'id_list_table')
+    #     rows = table.find_elements(By.TAG_NAME, 'tr')  #(1)
+    #     self.assertIn('1: Buy flowers', [row.text for row in rows])
+    #
+    #     # 页面下方还有一个文本框，用户可以输入待办事项
+    #     inputbox = self.browser.find_element(By.ID, 'id_new_item')
+    #     inputbox.send_keys('gift to girlfriend')  #(5)
+    #
+    #     # 页面再次更新，她的清单中显示了这两个待办事项
+    #     self.fail('Finish the test!')
+    #
+    #     # 页面中又显示了一个文本输入框，可以输入其他待办事项
+    #     # 他输入了 "Give a gift to Lisi"
+    #     inputbox = self.browser.find_element(By.ID, 'id_new_item')
+    #     inputbox.send_keys('Give a gift to Lisi')
+    #     inputbox.send_keys(Keys.ENTER)
+    #     time.sleep(1)
+    #
+    #     # 页面再次更新，显示了两个待办事项
+    #     table = self.browser.find_element(By.ID, 'id_list_table')
+    #     rows = table.find_elements(By.TAG_NAME, 'tr')
+    #     self.assertIn('1: Buy flowers', [row.text for row in rows])
+    #     self.assertIn('2: Give a gift to Lisi', [row.text for row in rows])
+    #
+    #     # 张三想知道这个网站是否会记住他的清单
+    #     # 他看到网站为他生成了一个唯一的URL
+    #     self.fail('Finish the test!')
 
     def tearDown(self):
         self.browser.quit()
@@ -77,7 +85,7 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         # 张三听说有一个在线待办事项的应用
         # 他查看了这个应用的首页
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
 
         # 他注意到页面标题和头部都包含了“To-Do”这个词
         self.assertIn('To-Do', self.browser.title)
@@ -109,7 +117,7 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys('gift to girlfriend')  # (5)
 
         # 页面再次更新，她的清单中显示了这两个待办事项
-        # self.fail('Finish the test!')
+        self.fail('Finish the test!')
 
         # 页面中又显示了一个文本输入框，可以输入其他待办事项
         # 他输入了 "Give a gift to Lisi"
@@ -126,7 +134,7 @@ class NewVisitorTest(unittest.TestCase):
 
         # 张三想知道这个网站是否会记住他的清单
         # 他看到网站为他生成了一个唯一的URL
-        # self.fail('Finish the test!')
+        self.fail('Finish the test!')
         # 他按了回车键后，页面更新了
         # 待办事项列表中显示了"1: Buy flowers"
         inputbox.send_keys(Keys.ENTER)  # (3)
@@ -156,4 +164,4 @@ class NewVisitorTest(unittest.TestCase):
         inputbox = self.browser.find_element(By.ID, 'id_new_item')
         inputbox.send_keys('Give a gift to Lisi')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(100)
+        time.sleep(1)
