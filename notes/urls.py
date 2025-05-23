d9fcea9e185e1716ad690e7a96db068f3c99981b
gaoyidan2004 <2022211446@bupt.cn>
2025-05-23 16:18:53 +0800

    message 15: new URL, view and template to display lists

--- notes/urls.py
+++ notes/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 """
 from django.urls import path
 from lists import views
+from django.urls import path
+from lists import views
 
 urlpatterns = [
     path('', views.home_page, name='home'),
+    path('lists/new', views.new_list, name='new_list'),
     path('lists/the-new-page/', views.view_list, name='view_list'),
 ]
