896751c2363235c098fc8b90a5b0ab9758d559b2
gaoyidan2004 <2022211446@bupt.cn>
2025-05-23 17:30:47 +0800

    message18: new URL and view for adding to existing lists. UT and FT pass

--- lists/views.py
+++ lists/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from lists.models import Item
-
+from lists.models import Item, List
 def home_page(request):
     if request.method == 'POST':
         Item.objects.create(text=request.POST['item_text'])
@@ -9,10 +8,24 @@ def home_page(request):
     return render(request, 'home.html')
 
 
-def view_list(request):
-    items = Item.objects.all()
-    return render(request, 'list.html', {'items': items})
+def view_list(request, list_id):
+    list_user = List.objects.get(id=list_id)
+    return render(request, 'list.html', {'list': list_user})
+
+
+
 
 def new_list(request):
-    Item.objects.create(text=request.POST['item_text'])
-    return redirect('/lists/the-new-page/')
+    list_user = List.objects.create()
+    Item.objects.create(text=request.POST['item_text'], list=list_user)
+    return redirect(f'/lists/{list_user.id}/')
+
+def test_redirects_after_POST(self):
+    response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
+    new_list = List.objects.first()
+    self.assertRedirects(response, f'/lists/{new_list.id}/')
+
+def add_item(request, list_id):
+    list_user = List.objects.get(id=list_id)
+    Item.objects.create(text=request.POST['item_text'], list=list_user)
+    return redirect(f'/lists/{list_user.id}/')
